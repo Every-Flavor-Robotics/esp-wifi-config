962d309a60c795b75d220bb83df0520d4e38e72f
Swapnil Pande <swapnil.pande98@gmail.com>
2023-11-15 17:47:19 -0500

    Added ability to deregister variables from variable_manager

--- include/variable_manager.h
+++ include/variable_manager.h
@@ -34,7 +34,26 @@ class VariableManager
   }
 
   // Method to add an item
-  void register_variable(T* item) { items.push_back(item); }
+  void register_variable(T* item)
+  {
+    items.push_back(item);
+    // Serial.println("Registerered Variable");
+  }
+
+  void deregister_variable(T* item)
+  {
+    // Serial.println("Deregistering Variable");
+    // Scan through the vector and remove the item
+    for (auto it = items.begin(); it != items.end(); ++it)
+    {
+      if (*it == item)
+      {
+        // Serial.println("Found item to remove");
+        items.erase(it);
+        break;
+      }
+    }
+  }
 
   // Method to get all items
   const std::vector<T*>& get_variables() const { return items; }
